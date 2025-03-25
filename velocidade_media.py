23c609061102255347f5885e095b889aa01b2f58
kely.vieira <kelysbl27@gmail.com>
2025-03-25 18:00:58 -0300

    adiciona calculo de velocidade

---
+++ velocidade_media.py
@@ -0,0 +1,7 @@
+distancia = float(input("Informe a distância: "))
+tempo = float(input("Informe o tempo necessario para a viagem: "))
+
+velocidade_media = distancia / tempo
+
+print("A velocidade media foi de {:.2f} km/h".format(velocidade_media))
+print(f"A velocidade media foi de {velocidade_media:.2f} km/h")
